d0f3ae38098823e6cb73c61935a26c3fbc0aef32
medyannikov <vmedyannikov76@gmail.com>
2023-02-17 16:46:46 +0300

    часть 4 руководства, конец дня

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
-from .models import Question
+from .models import Question, Choice
+from django.urls import reverse
 
 
 # from django.template import loader
@@ -21,9 +22,40 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = f'Вы смотрите на результаты опроса по id {question_id}'
-    return HttpResponse(response)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse(f'Вы голосуете по опросу id {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "Вы не выбирали вариант."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
