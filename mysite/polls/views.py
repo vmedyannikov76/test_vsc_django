8f96584341b0446b8a99f3eeb6dd70a5542436b6
medyannikov <vmedyannikov76@gmail.com>
2023-02-17 11:48:13 +0300

    пошел на обед 1702))

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.http import Http404
+from .models import Question
+# from django.template import loader
+
 
 def index(request):
-    return HttpResponse('Привет, здесь будет приложение опросов')
+    latest_question_list = Question.objects.order_by('pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
-# Create your views here.
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {'question': question})
+
+
+def results(request, question_id):
+    response = f'Вы смотрите на результаты опроса по id {question_id}'
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f'Вы голосуете по опросу id {question_id}')
